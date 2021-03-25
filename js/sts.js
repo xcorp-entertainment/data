e4e6fe9299683365a7671007023d3c9da2e8af45
xcorp-entertainment <81333699+xcorp-entertainment@users.noreply.github.com>
2021-03-25 11:02:19 +1100

    Update sts.js

--- js/sts.js
+++ js/sts.js
@@ -2,9 +2,11 @@ console.log('hello world!');
 let nv = document.getElementById('nav'),
 dd = nv.children;
 
+console.log(dd)
 dd[8].lastChild.classList.add('pull-right');
 
 const utils = {
 
 }
+
 
