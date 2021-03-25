b1d5632e9e92e9621457c4be36d5ab230ab35dae
angeal185 <beneaves01@hotmail.com>
2021-03-25 22:47:18 +1100

    update

--- js/sts.mjs
+++ js/sts.mjs
@@ -63,7 +63,7 @@ const utils = {
       x('i', {class: 'fa fa-bars'})
     ),
     toTop = x('button', {
-        class: 'to-top',
+        class: 'tt hidden',
         type: 'button',
         onclick(){
           utils.totop(0);
@@ -72,7 +72,7 @@ const utils = {
       x('i', {class: 'fa fa-chevron-up'})
     )
 
-    /*
+
     window.addEventListener('scroll', utils.debounce(function(evt){
       let top = window.pageYOffset || document.scrollTop;
 
@@ -84,7 +84,7 @@ const utils = {
       top = null;
       return;
     }, 250))
-    */
+
 
     document.body.append(ele,toTop);
     document.getElementById('cartsm').remove()
