d8b3112289dffb597e61fe3d6f2ab684b3646e87
xcorp-entertainment <81333699+xcorp-entertainment@users.noreply.github.com>
2021-03-25 11:21:05 +1100

    Update sts.js

--- js/sts.js
+++ js/sts.js
@@ -3,13 +3,31 @@ console.log('hello world!');
 
 const utils = {
   fixNav(){
+
     let nv = document.getElementById('nav'),
     dd = nv.children[8];
     dd.classList.add('pull-right');
+
+    utils.fixNav(nv)
+  },
+  addCams(nv){
+    let ele = document.createElement('li'),
+    anc = document.createElement('a');
+
+    ele.classList.add('nav-item');
+    ele.setAttribute('itemprop', 'url');
+    anc.href = 'https://www.xxxcams.chat/';
+    anc.target = '_blank';
+    anc.textContent = 'CAMS';
+    ele.setAttribute('itemprop', 'name');
+    ele.append(anc);
+    nv.append(ele);
+
   }
 }
 
 utils.fixNav()
+
 
 
 
