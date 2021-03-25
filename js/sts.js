03ffbfaeae617fb1513afed4a7227168a064ad1e
xcorp-entertainment <81333699+xcorp-entertainment@users.noreply.github.com>
2021-03-25 11:28:41 +1100

    Update sts.js

--- js/sts.js
+++ js/sts.js
@@ -1,33 +1,54 @@
-console.log('hello world!');
+console.log('app starting...');
 
+function docReady(fn) {
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+}
 
 const utils = {
   fixNav(){
 
-    let nv = document.getElementById('nav'),
-    dd = nv.children[8];
-    dd.classList.add('pull-right');
+    try {
 
-    utils.fixNav(nv)
+      let nv = document.getElementById('nav'),
+      dd = nv.children[8];
+      dd.classList.add('pull-right');
+
+      utils.addCams(nv);
+
+    } catch (err) {
+      console.error(err)
+    }
+
   },
   addCams(nv){
-    let ele = document.createElement('li'),
-    anc = document.createElement('a');
 
-    ele.classList.add('nav-item');
-    ele.setAttribute('itemprop', 'url');
-    anc.href = 'https://www.xxxcams.chat/';
-    anc.target = '_blank';
-    anc.textContent = 'CAMS';
-    ele.setAttribute('itemprop', 'name');
-    ele.append(anc);
-    nv.append(ele);
+    try {
+
+      let ele = document.createElement('li'),
+      anc = document.createElement('a');
+
+      ele.classList.add('nav-item');
+      ele.setAttribute('itemprop', 'url');
+      anc.href = 'https://www.xxxcams.chat/';
+      anc.target = '_blank';
+      anc.textContent = 'CAMS';
+      ele.setAttribute('itemprop', 'name');
+      ele.append(anc);
+      nv.append(ele);
+
+    } catch (err) {
+      console.error(err)
+    }
+
 
   }
 }
 
-utils.fixNav()
 
-
-
-
+docReady(function() {
+  utils.fixNav()
+});
